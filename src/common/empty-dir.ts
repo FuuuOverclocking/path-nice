3cf3452b1baa5e4a91adfde279536a2ebcbf0c20
Fuu <g.for.xyz@gmail.com>
2022-07-03 13:32:44 +0800

    feat(common): emptyDir now create dir if not exists

--- src/common/empty-dir.ts
+++ src/common/empty-dir.ts
@@ -1,8 +1,15 @@
 import type { FileSystem, PlatformPath } from './types.js';
 import { remove } from './remove.js';
+import { ensureDir } from './ensure.js';
 
 export async function emptyDir(path: PlatformPath, fs: FileSystem, target: string) {
-    const files: string[] = await (fs.promises as any).readdir(target);
+    let files!: string[];
+    try {
+        files = await (fs.promises as any).readdir(target);
+    } catch (e) {
+        await ensureDir(fs, target);
+        return;
+    }
     await Promise.all(
         files.map((f) => {
             return remove(fs, path.join(target, f));
